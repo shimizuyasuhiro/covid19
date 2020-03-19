0513e360ae2d905a3204ba8efb6aea55cc851ee5
shimizuyasuhiro <swimmy@live.jp>
2020-03-19 15:22:46 +0900

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -61,7 +61,7 @@ export default Vue.extend({
       Data,
       headerItem: {
         icon: 'mdi-chart-timeline-variant',
-        title: this.$t('都内の最新感染動向'),
+        title: this.$t('これは確認用にクローンしたサイトです！注！！'),
         date: Data.lastUpdate
       },
       newsItems: News.newsItems
@@ -70,7 +70,7 @@ export default Vue.extend({
   },
   head(): MetaInfo {
     return {
-      title: this.$t('これは確認ようにクローンしたサイトです！注！') as string
+      title: this.$t('テストサイトだよ！') as string
     }
   }
 })
